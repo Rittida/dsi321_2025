987b91bf2ec14c3194106dd183ad6aa25e3c219a
Ritthida Yingnaikiat <ritthida.pang@gmail.com>
2025-05-13 12:01:13 +0700

    Connect LakeFS to Streamlit

---
+++ visualization/app.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+import streamlit as st
+import pyarrow.parquet as pq
+import s3fs
+import time
+from zoneinfo import ZoneInfo
+from datetime import timedelta, datetime
+
+
+# Set up environments of LakeFS
+lakefs_endpoint = os.getenv("LAKEFS_ENDPOINT", "http://lakefs-dev:8000")
+ACCESS_KEY = os.getenv("LAKEFS_ACCESS_KEY")
+SECRET_KEY = os.getenv("LAKEFS_SECRET_KEY")
+
+# Setting S3FileSystem for access LakeFS
+fs = s3fs.S3FileSystem(
+    key=ACCESS_KEY,
+    secret=SECRET_KEY,
+    client_kwargs={'endpoint_url': lakefs_endpoint}
+)
+
+@st.cache_data()
+def load_data():
+    lakefs_path = "s3://air-quality/main/airquality.parquet/year=2025"
+    data_list = fs.glob(f"{lakefs_path}/*/*/*/*")
+    df_all = pd.concat([pd.read_parquet(f"s3://{path}", filesystem=fs) for path in data_list], ignore_index=True)
+    df_all['lat'] = pd.to_numeric(df_all['lat'], errors='coerce')
+    df_all['long'] = pd.to_numeric(df_all['long'], errors='coerce')
+    df_all['year'] = df_all['year'].astype(int) 
+    df_all['month'] = df_all['month'].astype(int)
+    df_all.drop_duplicates(inplace=True)
+    df_all['PM25.aqi'] = df_all['PM25.aqi'].mask(df_all['PM25.aqi'] < 0, pd.NA)
+    # Fill value "Previous Record" Group By stationID
+    df_all['PM25.aqi'] = df_all.groupby('stationID')['PM25.aqi'].transform(lambda x: x.fillna(method='ffill'))
+    return df_all
+
+df = load_data()
+st.write(df.head(10))
